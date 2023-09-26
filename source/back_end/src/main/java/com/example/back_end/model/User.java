4c85512ba4ec407d5decdfd894806e84dce5537b
KosoneNatsuki <gpc.7.ntk.2.4@gmail.com>
2023-09-26 12:59:13 +0900

    ユーザー新規登録実装

---
+++ source/back_end/src/main/java/com/example/back_end/model/User.java
@@ -0,0 +1,21 @@
+package com.example.back_end.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter // フィールドのgetメソッド(id, content)を自動生成
+@Setter // フィールドのsetメソッド(id, content)を自動生成
+@Entity // データの入れ物であるEntityクラスであることを指定
+
+public class User {
+    @Id // Entityの主キーを設定する
+    @GeneratedValue // Entityの値を自動採番する
+    
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
