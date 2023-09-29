1c66ba90db801ff505972482a5413e08173cc2a6
KosoneNatsuki <gpc.7.ntk.2.4@gmail.com>
2023-09-29 20:38:41 +0900

    ホーム画面更新

--- source/tunag_app_clone/lib/src/home.dart
+++ source/tunag_app_clone/lib/src/home.dart
@@ -13,6 +13,29 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> {
   var logger = Logger();
 
+  // ボタンとテキスト作成関数
+  Widget buttonTextItem(IconData icon, String label, VoidCallback onPressed) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          icon: Icon(icon),
+          color: Colors.black,
+          iconSize: 28,
+          onPressed: onPressed,
+        ),
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 10,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,8 +74,6 @@ class HomePageState extends State<HomePage> {
                 hintMaxLines: 5,
                 prefixIcon: IconButton(
                   onPressed: () {
-                    // デバック
-                    // アイコンが押下されたときの処理
                     logger.i(" 検索アイコン押下されました");
                   },
                   icon: const Icon(
@@ -72,103 +93,196 @@ class HomePageState extends State<HomePage> {
             ),
             child: Align(
               alignment: Alignment.centerLeft,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.border_color_rounded),
-                    color: Colors.black,
-                    iconSize: 28,
-                    onPressed: () {
-                      // アイコンが押下されたときの処理
-                      logger.i("投稿ボタン押下");
-                    },
-                  ),
-                  const Text(
-                    "投稿する",
-                    style: TextStyle(
-                        fontSize: 10,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
+              child: buttonTextItem(
+                Icons.border_color_rounded,
+                "投稿する",
+                () {
+                  logger.i("投稿ボタン押下");
+                },
               ),
             ),
           ),
           Container(
             width: double.infinity, // 横幅いっぱいに広げる
             height: 1, // 線の高さ
-            color: Colors.grey, // 線の色
+            color: const Color.fromARGB(255, 181, 180, 180), // 線の色
           ),
 
-          ListView(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 20,
-                  horizontal: 20,
-                ),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Column(
-                    children: [
-                      const Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Icon(
-                            // 仮
-                            Icons.favorite,
-                            color: Colors.pink,
-                            size: 40,
-                          ),
-                          SizedBox(width: 15),
-                          Text(
-                            "テスト1",
-                            style: TextStyle(fontSize: 15),
-                          )
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 20,
-                          horizontal: 20,
+          // 投稿エリア
+          SizedBox(
+            width: double.infinity, // 幅を親要素に合わせる
+            height: 520, // メイン高さを指定
+            child: ListView(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 20,
+                    horizontal: 20,
+                  ),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Column(
+                      children: [
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Icon(
+                              // 仮
+                              Icons.favorite,
+                              color: Colors.pink,
+                              size: 40,
+                            ),
+                            SizedBox(width: 15),
+                            Text(
+                              "テスト1",
+                              style: TextStyle(fontSize: 15),
+                            )
+                          ],
                         ),
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text(
-                                "タイトル",
-                                style: TextStyle(
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.bold,
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 20,
+                            horizontal: 20,
+                          ),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "タイトル",
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              ),
-                              const SizedBox(height: 20),
-                              Image.asset("images/Test1.jpg"), // 仮
-                              const SizedBox(height: 15),
-                              const Text("おすすめのお店です。"),
-                              const SizedBox(height: 15),
-                              const Icon(
-                                Icons.favorite,
-                                color: Colors.pink,
-                                size: 30,
-                              ),
-                            ],
+                                const SizedBox(height: 20),
+                                Image.asset("images/Test1.jpg"), // 仮
+                                const SizedBox(height: 15),
+                                const Text("おすすめのお店です。"),
+                                const SizedBox(height: 15),
+                                const Icon(
+                                  Icons.favorite,
+                                  color: Colors.pink,
+                                  size: 30,
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                        // 以下レイアウト確認の為記載_後ほど削除
+                        // const Row(
+                        //   mainAxisAlignment: MainAxisAlignment.start,
+                        //   children: [
+                        //     Icon(
+                        //       // 仮
+                        //       Icons.favorite,
+                        //       color: Colors.pink,
+                        //       size: 40,
+                        //     ),
+                        //     SizedBox(width: 15),
+                        //     Text(
+                        //       "テスト1",
+                        //       style: TextStyle(fontSize: 15),
+                        //     )
+                        //   ],
+                        // ),
+                        // Padding(
+                        //   padding: const EdgeInsets.symmetric(
+                        //     vertical: 20,
+                        //     horizontal: 20,
+                        //   ),
+                        //   child: Align(
+                        //     alignment: Alignment.centerLeft,
+                        //     child: Column(
+                        //       crossAxisAlignment: CrossAxisAlignment.start,
+                        //       children: [
+                        //         const Text(
+                        //           "タイトル",
+                        //           style: TextStyle(
+                        //             fontSize: 25,
+                        //             fontWeight: FontWeight.bold,
+                        //           ),
+                        //         ),
+                        //         const SizedBox(height: 20),
+                        //         Image.asset("images/Test1.jpg"), // 仮
+                        //         const SizedBox(height: 15),
+                        //         const Text("おすすめのお店です。"),
+                        //         const SizedBox(height: 15),
+                        //         const Icon(
+                        //           Icons.favorite,
+                        //           color: Colors.pink,
+                        //           size: 30,
+                        //         ),
+                        //       ],
+                        //     ),
+                        //   ),
+                        // ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
+
           Container(
             width: double.infinity, // 横幅いっぱいに広げる
             height: 1, // 線の高さ
-            color: Colors.grey, // 線の色
+            color: const Color.fromARGB(255, 181, 180, 180), // 線の色
+          ),
+
+          // ヘッダー
+          Align(
+            alignment: Alignment.center,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // ホームボタン
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 0,
+                    horizontal: 30,
+                  ),
+                  child: buttonTextItem(
+                    Icons.border_color_rounded,
+                    "ホーム",
+                    () {
+                      logger.i("ホームボタン押下");
+                    },
+                  ),
+                ),
+
+                // マイページボタン
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 0,
+                    horizontal: 30,
+                  ),
+                  child: buttonTextItem(
+                    Icons.border_color_rounded,
+                    "マイページ",
+                    () {
+                      logger.i("マイページボタン押下");
+                    },
+                  ),
+                ),
+                // チャットボタン
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 0,
+                    horizontal: 30,
+                  ),
+                  child: buttonTextItem(
+                    Icons.border_color_rounded,
+                    "チャット",
+                    () {
+                      logger.i("チャットボタン押下");
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ]),
       ),
